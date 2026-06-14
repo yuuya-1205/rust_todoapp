233e649fb323f927c0a3d1fe7508d1741422fa5a
ushi <>
2026-06-14 09:51:43 +0900

    feature: first step todoapp

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    println!("題名を入力してください");
+    let mut title = String::new();
+    std::io::stdin().read_line(&mut title).unwrap();
+
+    println!("本文を入力してください");
+    let mut content = String::new();
+    std::io::stdin().read_line(&mut content).unwrap();
+
+    println!("題名:{}", title);
+    println!("本文:{}", content);
 }
